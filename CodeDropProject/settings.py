35175e2e165b3d0f3c33abc0eadb6c34b372a89f
senymama <senymama228@gmail.com>
2024-06-30 22:50:23 +0300

    Security Update Getting the SECRET_KEY from environment variables in settings.py

--- CodeDropProject/settings.py
+++ CodeDropProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ti8v5q0h)tx2iiso6l4nra0l#bd4$)1-@mmb_w09)bzc1ug@g2'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
